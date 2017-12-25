1e40224f4d511be8a1e58eab1347b87592f1b393
Lukas Solanka <lsolanka@gmail.com>
2017-12-25 22:26:46 +0000

    add gennodejs 2.0.1-p1

--- cmake/projects/ros-gennodejs/hunter.cmake
+++ cmake/projects/ros-gennodejs/hunter.cmake
@@ -10,16 +10,16 @@ include(hunter_pick_scheme)
 
 hunter_add_version(
     PACKAGE_NAME
-    gennodejs
+    ros-gennodejs
     VERSION
-    devel
+    2.0.1-p1
     URL
-    "https://github.com/lsolanka/gennodejs/archive/hunterise.tar.gz"
+    "https://github.com/lsolanka/gennodejs/archive/2.0.1-p1.tar.gz"
     SHA1
-    b4cc64e3a292356b8eeeea2f2a2951c68aa1e6c3
+    241a3f06eacca1eae5bc8b7eeb7a56e8a8c1bf88
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
-hunter_cacheable(gennodejs)
-hunter_download(PACKAGE_NAME gennodejs)
+hunter_cacheable(ros-gennodejs)
+hunter_download(PACKAGE_NAME ros-gennodejs)
 
