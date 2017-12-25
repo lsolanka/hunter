d295fe53a8f2413366a5941298ab28ddb1473c02
Lukas Solanka <lsolanka@gmail.com>
2017-12-25 22:00:43 +0000

    add ros-genmsg 0.5.9-p1

--- cmake/projects/ros-genmsg/hunter.cmake
+++ cmake/projects/ros-genmsg/hunter.cmake
@@ -10,16 +10,16 @@ include(hunter_pick_scheme)
 
 hunter_add_version(
     PACKAGE_NAME
-    genmsg
+    ros-genmsg
     VERSION
-    devel
+    0.5.9-p1
     URL
-    "https://github.com/lsolanka/genmsg/archive/hunterise.tar.gz"
+    "https://github.com/lsolanka/genmsg/archive/0.5.9-p1.tar.gz"
     SHA1
-    d20317891b0cdcc6750ca8e7807d709a6a93be35
+    1ef00861ce47c4b83441e3c2726bbf2f61413c62
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
-hunter_cacheable(genmsg)
-hunter_download(PACKAGE_NAME genmsg)
+hunter_cacheable(ros-genmsg)
+hunter_download(PACKAGE_NAME ros-genmsg)
 
