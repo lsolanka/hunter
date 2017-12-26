663071db589aeb673249f7c2f82275453c18da48
Lukas Solanka <lsolanka@gmail.com>
2017-12-26 15:59:10 +0000

    add ros-ros_comm 1.12.2-p1

--- cmake/projects/ros-ros_comm/hunter.cmake
+++ cmake/projects/ros-ros_comm/hunter.cmake
@@ -7,19 +7,23 @@ include(hunter_add_version)
 include(hunter_cacheable)
 include(hunter_download)
 include(hunter_pick_scheme)
+include(hunter_cmake_args)
 
 hunter_add_version(
     PACKAGE_NAME
-    ros_comm
+    ros-ros_comm
     VERSION
-    devel
+    1.12.2-p1
     URL
-    "https://github.com/lsolanka/ros_comm/archive/hunterise.tar.gz"
+    "https://github.com/lsolanka/ros_comm/archive/1.12.2-p1.tar.gz"
     SHA1
-    009c5fabb83a448dbfc63be480cb394ceb32bab4
+    2e26b9f5fdac810eb59c97d69ebe989a9e0a384a
 )
 
+hunter_cmake_args(ros-ros_comm
+    CMAKE_ARGS CATKIN_ENABLE_TESTING=OFF)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
-hunter_cacheable(ros_comm)
-hunter_download(PACKAGE_NAME ros_comm)
+hunter_cacheable(ros-ros_comm)
+hunter_download(PACKAGE_NAME ros-ros_comm)
 
