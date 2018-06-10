c53ce00e3145d6b968c8bcde09e5e36e6105ac01
Lukas Solanka <lsolanka@gmail.com>
2018-06-10 20:02:49 +0100

    add roboclaw 0.1.1

--- cmake/projects/roboclaw/hunter.cmake
+++ cmake/projects/roboclaw/hunter.cmake
@@ -19,6 +19,17 @@ hunter_add_version(
     "1fc551800905b3519f34a9e6eb15207c037d08ea"
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    roboclaw
+    VERSION
+    0.1.1
+    URL
+    "https://github.com/lsolanka/roboclaw/archive/v0.1.1.tar.gz"
+    SHA1
+    "6499031e9bdeb985ee79cb77e76921c4783f5da9"
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(roboclaw)
 hunter_download(PACKAGE_NAME roboclaw)
--- examples/roboclaw/main.cpp
+++ examples/roboclaw/main.cpp
@@ -1,38 +1,9 @@
-#include <boost/program_options.hpp>
-#include <boost/asio/serial_port.hpp>
-
-#include <roboclaw/logging.hpp>
 #include <roboclaw/io/io.hpp>
 #include <roboclaw/io/read_commands.hpp>
 #include <roboclaw/io/write_commands.hpp>
 
-namespace po = boost::program_options;
 namespace read_commands = roboclaw::io::read_commands;
 namespace write_commands = roboclaw::io::write_commands;
-
-BOOST_LOG_GLOBAL_LOGGER_INIT(logger, logger_t)
-{
-    namespace expr = boost::log::expressions;
-    logger_t lg;
-    boost::log::add_common_attributes();
-    boost::log::add_file_log(
-                boost::log::keywords::file_name = "log.txt",
-                boost::log::keywords::format = (
-                        expr::stream << expr::format_date_time<     boost::posix_time::ptime >("TimeStamp", "%Y-%m-%d %H:%M:%S")
-                        << " [" << expr::attr<     boost::log::trivial::severity_level >("Severity") << "] "
-                        << expr::smessage
-            )
-    );
-    boost::log::add_console_log(
-                std::cout,
-                boost::log::keywords::format = (
-                        expr::stream << expr::format_date_time<     boost::posix_time::ptime >("TimeStamp", "%Y-%m-%d %H:%M:%S")
-                        << " [" << expr::attr<     boost::log::trivial::severity_level >("Severity") << "] "
-                        << expr::smessage
-            )
-    );
-    return lg;
-}
 
 int main(int argc, char** argv)
 {
